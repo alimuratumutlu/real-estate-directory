4d378c4aa0c9cf0f0a94ac9d7c5d2968092c2325
Murat Umutlu <muratumutlu@gmail.com>
2020-05-13 03:21:15 +0300

    mnor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import Item from "./components/Item";
 
+import { theme } from "@chakra-ui/core";
+
+// Let's say you want to add custom colors
+const customTheme = {
+  ...theme,
+  colors: {
+    ...theme.colors,
+    brand: {
+      900: "#1a365d",
+      800: "#153e75",
+      700: "#2a69ac",
+    },
+  },
+};
+
 import {
   Avatar,
   Badge,
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,70 +1,24 @@
 import React from "react";
-import { IoMdCart } from "react-icons/io";
+import { theme } from "@chakra-ui/core";
+import { Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/core";
 
-import { IconButton } from "gestalt";
-import "gestalt/dist/gestalt.css";
-
-export default function Header(props) {
-  const [isOpen, setOpen] = React.useState(false);
-
+const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-white fixed-top shadow">
-      <div className="container">
-        <a className="navbar-brand" href="#">
-          Villa Cart
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <input
-              className="form-control input-long"
-              type="text"
-              placeholder="You may search a Villa name, or use buttons to filter and to see on the map"
-              size="100"
-            />
-          </ul>
-          <span className="d-inline">
-            <IconButton
-              accessibilityLabel="see more"
-              accessibilityHaspopup
-              accessibilityExpanded={isOpen}
-              icon="filter"
-              onClick={() => setOpen(!isOpen)}
-            />
-          </span>
-          <span className="d-inline">
-            <IconButton
-              accessibilityLabel="see more"
-              accessibilityHaspopup
-              accessibilityExpanded={isOpen}
-              icon="location"
-              onClick={() => setOpen(!isOpen)}
-              className="d-inline"
-            />
-          </span>
-          <span className="d-inline">
-            <IconButton
-              accessibilityLabel="see more"
-              accessibilityHaspopup
-              accessibilityExpanded={isOpen}
-              icon="shopping-bag"
-              onClick={() => setOpen(!isOpen)}
-              className="d-inline"
-            />
-          </span>
-        </div>
-      </div>
-    </nav>
+    <Menu>
+      <MenuButton as={Button} rightIcon="chevron-down">
+        Actions
+      </MenuButton>
+      <MenuList>
+        <MenuItem>Download</MenuItem>
+        <MenuItem>Create a Copy</MenuItem>
+        <MenuItem>Mark as Draft</MenuItem>
+        <MenuItem>Delete</MenuItem>
+        <MenuItem as="a" href="#">
+          Attend a Workshop
+        </MenuItem>
+      </MenuList>
+    </Menu>
   );
-}
+};
+
+export default Header;
